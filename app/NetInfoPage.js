ee673c5a612c2c94f4d5ba254a9365afe5ed1ce8
mxb <928975247@qq.com>
2017-03-23 17:38:22 +0800

    完成网络状态监听基本功能

--- app/NetInfoPage.js
+++ app/NetInfoPage.js
@@ -12,18 +12,76 @@ import {
     View,
     Text,
     StyleSheet,
-    Button
+    Button,
+    NetInfo,
+    ToastAndroid
 }from 'react-native';
 
-export default class NetInfoPage extends Component{
-    constructor(props){
+export default class NetInfoPage extends Component {
+    constructor(props) {
         super(props);
+        this.state = {
+            isConnected: null,
+            connectionInfo: null,
+            isConnectionExpensive:null
+        };
     }
 
-    render(){
-        return(
+    componentDidMount() {
+        NetInfo.isConnected.addEventListener(
+            'change',
+            this._handleConnectivityChange.bind(this)
+        );
+        //检测网络是否连接
+        NetInfo.isConnected.fetch().done(
+            (isConnected) => {
+                this.setState({isConnected});
+            }
+        );
+        //检测网络连接信息
+        NetInfo.fetch().done(
+            (connectionInfo) => {
+                this.setState({connectionInfo});
+            }
+        );
+    }
+
+    componentWillUnmount() {
+        NetInfo.isConnected.removeEventListener(
+            'change',
+            this._handleConnectivityChange
+        );
+    }
+
+    _handleConnectivityChange(isConnected) {
+        ToastAndroid.show((isConnected ? 'online' : 'offline'), ToastAndroid.SHORT);
+        this.setState({
+            isConnectionExpensive:NetInfo.isConnectionExpensive,
+            isConnected:isConnected,
+        });
+        //检测网络连接信息
+        NetInfo.fetch().done(
+            (connectionInfo) => {
+                this.setState({connectionInfo});
+            }
+        );
+    }
+
+    render() {
+        return (
             <View style={[styles.container]}>
                 <Text style={[styles.text]}>网络状态页</Text>
+                <View >
+                    <Text style={styles.welcome}>
+                        当前的网络状态：{this.state.isConnected ? '网络在线' : '离线'}
+                    </Text>
+                    <Text style={styles.welcome}>
+                        当前网络连接类型：{this.state.connectionInfo}
+                    </Text>
+                    <Text style={styles.welcome}>
+                        当前连接网络是否计费：{this.state.isConnectionExpensive === true ? '需要计费' : '不要'}
+                    </Text>
+                </View>
             </View>
         );
     }
@@ -36,7 +94,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#CCC',
     },
-    text:{
-        color:'#520',
+    text: {
+        color: '#520',
     }
 });
